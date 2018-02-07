56eb1100d9a1d2c6d9e680311099a354a1f4a8f4
Eugene Sadovoi <evgeni@eniks.com>
2018-02-07 12:27:20 -0500

    Update ContainerLifetimeManager.cs

--- src/Container/Lifetime/ContainerLifetimeManager.cs
+++ src/Container/Lifetime/ContainerLifetimeManager.cs
@@ -5,33 +5,20 @@ namespace Unity.Container.Lifetime
 {
     /// <summary>
     /// Internal container lifetime manager. 
-    /// This manager distinguishes internal registration from user mode registration.
     /// </summary>
-    /// <remarks>
-    /// Works like the ExternallyControlledLifetimeManager, but uses 
-    /// regular instead of weak references
-    /// </remarks>
     internal class ContainerLifetimeManager : LifetimeManager
     {
-        private object _value;
-
         public override object GetValue(IBuilderContext context = null)
         {
-            return _value;
+            return context.Container;
         }
 
         public override void SetValue(object newValue, IBuilderContext context = null)
         {
-            _value = newValue;
         }
 
         public override void RemoveValue(IBuilderContext context = null)
         {
-        }
-
-        public object Resolve(IBuilderContext _)
-        {
-            return _value;
         }
 
         protected override LifetimeManager OnCreateLifetimeManager()
